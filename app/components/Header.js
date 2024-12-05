b4d4607e4764131c122f93403fc31af568f500b5
samuelajayii <ajayisamuel023@gmail.com>
2024-12-05 15:29:03 +0100

    created a searchform component, finished the ternary operator in the header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, {  } from 'react';
+import React, { } from 'react';
 import { auth, signOut, signIn } from "@/auth"
+
 
 
 
@@ -18,27 +19,34 @@ const Header = async () => {
                     <Image src='/logo-design.jpg' width={40} height={40} alt='logo' />
                 </div>
 
-                <ul className='flex items-center gap-5'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>
-                        {session ? (
-                            <div className='flex items-center gap-3'>
+                <div className='flex items-center gap-5'>
+
+                    <div>
+                        {session && session?.user ? (
+                            <div className='flex items-center gap-5 text-black'>
                                 <h1>Welcome {user?.name || "Guest"}</h1>
-                                <form action={async () => { "use server"; await signOut() }}>
+                                <Link href="/"><span>Home</span></Link>
+                                <Link href="/profile/create"><span>Create Travelogue</span></Link>
+                                <Link href={`/user/${session?.id}`}><span>Profile</span></Link>
+
+                                <form action={async () => { "use server"; await signOut({redirectTo: "/"}) }}>
                                     <button type="submit">Sign Out</button>
                                 </form>
                             </div>
                         ) : (
+                            <div className='flex items-center gap-5'>
+                                <div>Home </div>
+                                <div>About </div>
+                                <div>Contact </div>
+                                <form action={async () => { "use server"; await signIn("google", {redirectTo: "/home"}) }}>
+                                    <button type="submit">Sign In</button>
+                                </form>
+                            </div>
 
-                            <form action={async () => { "use server"; await signIn("google") }}>
-                                <button type="submit">Sign In</button>
-                            </form>
                         )}
 
-                    </li>
-                </ul>
+                    </div>
+                </div>
 
 
             </nav>
